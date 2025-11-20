d6dc283c309ce540adc1566f36020979dc6508c0
santiagovalen16 <santiagovalen16@gmail.com>
2025-11-19 21:45:33 -0500

    actualizacion del DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RoleSeeder::class,        // Primero los roles
             AdminUserSeeder::class,   // Luego el usuario administrador
+            UserSeeder::class,        // Luego los usuarios de prueba
             CuentasCobroSeeder::class, // Finalmente las cuentas de cobro
             
         ]);
